7fcc37b2705be0cd64b03e3cb44e0d4b3e1736ca
Memo <memosoft@ya.ru>
2024-06-03 21:55:34 +0300

    init: cosmetic changes

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            AWS shop by @MemoS77
           </Link>
         </Typography>
 
