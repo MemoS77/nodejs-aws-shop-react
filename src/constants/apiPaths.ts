d65c153bc0d7747eff75018ea78a1fdbb90b016e
Memo <memosoft@ya.ru>
2024-07-19 17:52:53 +0300

    feat: api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,9 @@
 const API_PATHS = {
-  //product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   product: "https://ppg1z1ffui.execute-api.eu-west-1.amazonaws.com/prod",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://e5w2p7cld7.execute-api.eu-west-1.amazonaws.com/prod",
   import: "https://1kzgs1hi1j.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://e5w2p7cld7.execute-api.eu-west-1.amazonaws.com/prod",
 };
 
 export default API_PATHS;
